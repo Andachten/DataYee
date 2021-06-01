f4af97d30434b0d8020d88753da29f1cb5905c1a
Andachten <50283838+Andachten@users.noreply.github.com>
2021-06-01 16:22:08 +0800

    Merge remote-tracking branch 'origin/main' into main

--- test/src/clusterscore.py
+++ test/src/clusterscore.py
@@ -7,6 +7,9 @@ Created on Tue Jun  1 09:09:11 2021
 from sklearn.neighbors import KernelDensity
 import numpy as np
 from scipy.signal import find_peaks
+from tslearn.metrics import cdist_dtw
+from loadjpk import forcecurve,loadjpkfile,zipfileopera
+from datapro_new import *
 def WRC_transformer(f,x,thr=20):
     b,gama = 0.11e-9,41/180*np.pi
     kb = 1.38e-23
@@ -26,7 +29,6 @@ def Lc_transformer(data_x,data_y,plottype='hist'):
     x_ = np.arange(int(x.min()),int(x.max()))
     log_dens = kde.score_samples(x_.reshape(-1,1))
     p,_ = find_peaks(np.exp(log_dens)/np.exp(log_dens).max(),height=0.15,distance=5,prominence=0.1)
-    return find_peaks(np.exp(log_dens)/np.exp(log_dens).max(),height=0.15,distance=5)
     lc = x_[p]
     dlc = np.diff(lc,prepend=0)
     f_max = np.array([])
@@ -37,5 +39,28 @@ def Lc_transformer(data_x,data_y,plottype='hist'):
             index = np.where(lc[i]<x_)[0]
         f_max = np.append(f_max,f[index].max())
     return dlc,f_max
-def wlc_dist(s1,s2):
+def wlc_dist(s1,s2,dlc_thre=5,f_thre=30):
+    s1,s2 = np.delete(s1,np.where(s1==0)[0]),np.delete(s2,np.where(s2==0)[0])
+    s1_dlc,s1_f = s1[:len(s1)//2],s1[len(s1)//2:]
+    s2_dlc,s2_f = s2[:len(s1)//2],s2[len(s1)//2:]
+    return cdist_dtw(s1_dlc,s2_dlc),cdist_dtw(s1_f,s2_f)
     pass
+if __name__=='__main__':
+    ljp = loadjpkfile(r'E:\ZB\jpkdata\20201201-COH-(I29)3-NGL-0_4UMS')
+    fc = forcecurve()
+    arr = np.array([])
+    for i,data in enumerate(ljp):
+        fc.data = data
+        noise_down(fc)
+        cal_baseline_y(fc)
+        cal_baseline_x(fc)
+        data = fc.get_prodata()['retract']
+        data_x,data_y = data['measuredHeight'].reshape(-1)*1e9,data['vDeflection'].reshape(-1)*1e12
+        dlc,f_max = Lc_transformer(data_x,data_y)
+        res = np.append(dlc,f_max)
+        res = np.pad(res,(0,50-len(res)))
+        if len(arr)==0:
+            arr = res
+        else:
+            arr = np.vstack((arr,res))
+        
