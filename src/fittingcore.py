d5a2eda69069184970b56f12b5bfd706e2134e09
Andachten <50283838+Andachten@users.noreply.github.com>
2021-06-09 11:21:28 +0800

    114

--- src/fittingcore.py
+++ src/fittingcore.py
@@ -12,10 +12,19 @@ def BE(x_arr,x_beta,k_off):
     f_beta = kb*T/x_beta
     F_arr = f_beta*np.log(x_arr/f_beta/k_off)
     return F_arr
-def Friddle():
-    pass
-def DHS(x_arr,x_beta,k_off,dG):
-    Feq = np.sqrt(2*k_off*dG)
+def Friddle(x_arr,x_beta,k_off,Feq):
     f_beta = kb*T/x_beta
-    F_arr = Feq+f_beta*(1+np.e*(-gama)*x_arr/k_off/Feq/f_beta)
+    F_arr = Feq + f_beta*np.log(1+np.e**(-1*gama)*x_arr/(k_off*f_beta))
+    return F_arr
+def DHS(x_arr,x_beta,k_off,dG):
     pass
+def fit(x_arr,y_arr,bounds,methods='BE',scale_factor=0.5):
+    while True:
+        inputs = list(product(*[np.linspace(x,y) for x,y in bounds]))
+        if methods=='BE':
+            y_pre = BE(x_arr,*)
+            pass
+        elif methods == 'DHS':
+            pass
+        elif methods == 'FD':
+            pass
