99607808e8ca16b2b8cef79c93c3508d33006b34
Andachten <1311343971@qq.com>
2021-09-17 14:52:10 +0800

    Create bugfix0918.py

---
+++ script/bugfix0918.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 17 14:39:32 2021
+
+@author: zbin
+"""
+
+
+class bugfix0918():
+    def __init__(self,zpo,ljp,fc):
+        self.zpo = zpo
+        self.ljp = ljp
+        self.fc = fc
+    def run(self,index):
+        self.fc.clean_force()
+        self.zpo.changingforce(self.fc)
+    def end(self):
+        self.zpo.changedforce()
